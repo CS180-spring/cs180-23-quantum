75ddc2568202862f50180a1c66b6fcafd5731f37
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-04-23 14:03:29 -0700

    Layout for Frontend Completed, Light and Dark Mode Completed, Responsiveness Completed, Mobile Completed and small changes to overall frontend architecture

--- Frontend/QuantumDB/tailwind.config.js
+++ Frontend/QuantumDB/tailwind.config.js
@@ -1,11 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+
+    // enable dark mode via class strategy
+    darkMode: 'class',
+
+    theme: {
+        extend: {
+            colors: {
+                black: '#09090c',
+                darkGray: '#121212',
+
+                brightRed: 'hsl(12, 88%, 59%)',
+                brightRedLight: 'hsl(12, 88%, 69%)',
+                brightRedSupLight: 'hsl(12, 88%, 95%)',
+                
+                darkPurple: 'hsl(262, 39%, 33%)',
+            },
+        },
+    },
+    plugins: [],
 }
